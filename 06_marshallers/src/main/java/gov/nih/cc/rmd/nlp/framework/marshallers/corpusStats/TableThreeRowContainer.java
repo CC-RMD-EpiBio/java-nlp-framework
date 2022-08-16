81b5a8e5dbc159d6f0001c434c4ab811edde81fe
Guy Divita <divitag2@nih.gov>
2022-08-16 17:56:18 -0400

    stripped placeholder copyright

--- 06_marshallers/src/main/java/gov/nih/cc/rmd/nlp/framework/marshallers/corpusStats/TableThreeRowContainer.java
+++ 06_marshallers/src/main/java/gov/nih/cc/rmd/nlp/framework/marshallers/corpusStats/TableThreeRowContainer.java
@@ -1,24 +1,3 @@
-/*******************************************************************************
- * ---------------------------------------------------------------------------
- *                                   NIH Clinical Center 
- *                             Department of Rehabilitation 
- *                        Epidemiology and Biostatistics Branch 
- *                                         2019
- *    
- *  This work is licensed under the Creative Commons Attribution 4.0 International License. 
- *  
- *  This license allows you to use, share and  adapt for any purpose, provided:
- *     Provide attribution to the creators of this work within your work.
- *     Indicate if changes were made to this work.
- *     No claim to merchantability, implied warranty, or liability can be made.
- *     
- *   When attributing this code, please make reference to
- *    [citation/url here] .  
- *    
- *     In the absence of a specific paper or url listed above, reference http://clinicalcenter.nih.gov/rmd/eb/nlp
- *  
- *  To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
- ******************************************************************************/
 // =======================================================
 /**
  * TableThreeRowContainer.java [Summary here]
